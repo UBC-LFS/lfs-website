33b78863997271dabe3fbfd1b61e8c4f33dd03c1
Justin Lee <justin.lee@ubc.ca>
2016-11-21 15:54:18 -0800

    making progress on featured profiles

--- _assets/featured-profiles.js
+++ _assets/featured-profiles.js
@@ -2,5 +2,24 @@ jQuery(document).ready(function ($) {
     var IDs = $("#featured div[id]")         
     .map(function() { return this.id; })
     .get();
+
     console.log(IDs);
+
+    var span12 = "<div class='span12' id='span12-0'></div>";
+    for (i = 0; i<IDs.length; i++) {
+        var span4 = '<div class="span4"' + 'id="span4-' + i + '"' + '></div>';
+        if (i===0) {
+            $("#featured").append(span12)
+        }
+        else if (i%3===0) {
+            span12= '<div class="span12"' + 'id="span12-' + i + '"' + '></div>';
+        }
+        var span12id = "#span12-" + i;
+        var span4id = "#span4-" + i;
+
+        $("#span12-0").append(span4);
+        $(span4id).append($(IDs[i]));
+
+        //$(span4id).append($(IDs[i]));
+    }
 });
