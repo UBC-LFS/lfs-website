107df33284a4e46dd8348a95d6dd1cad403c1919
Lee <justinlee0022@gmail.com>
2016-11-28 15:59:22 -0800

    Changed featured profiles to be more user friendly

--- _assets/featured-profiles.js
+++ _assets/featured-profiles.js
@@ -1,17 +1,13 @@
 jQuery(document).ready(function ($) {
-  var IDs = $("#featured div[id]")         
-    .map(function() { return this.id; })
-    .get();
-    for (i = 0; i<IDs.length; i++) {
+    $( ".span4" ).each(function(index) {
+         $( this ).addClass("index-" + Math.floor(index/3));
+    });
+    var numberOfProfiles = $('.span4').length;
+    var span12 = '<div class="span12" style="margin-left: 0;"></div>';
+    for (i=0; i<=numberOfProfiles; i++) {
         if (i%3===0) {
-            var span12= '<div class="span12 no-margin"' + 'id="span12-' + Math.floor(i/3) + '"' + '></div>';
-            $("#featured").append(span12);
+            $('.index-' + Math.floor(i/3)).wrapAll(span12);
         }
-        var span4 = '<div class="span4"' + 'id="span4-' + i + '"' + '></div>';
-        var span12id = "#span12-" + Math.floor(i/3);
-        var span4id = "#span4-" + i;
-        var profileID = "#" + IDs[i];
-        $(span12id).append(span4);
-        $(span4id).append($(profileID));
     }
+    $("img").attr("id", "featured");
 });
