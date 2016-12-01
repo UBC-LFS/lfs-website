531eaeedf79db3de4a2a41d26e83bb04a253447a
Lee <justinlee0022@gmail.com>
2016-12-01 10:26:15 -0800

    made progress on news, fixed featured profiles padding

--- _assets/featured-profiles.js
+++ _assets/featured-profiles.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
          $( this ).addClass("index-" + Math.floor(index/3));
     });
     var numberOfProfiles = $('.span4').length;
-    var span12 = '<div class="span12" style="margin-left: 0;"></div>';
+    var span12 = '<div class="span12 border-bottom" style="margin-left: 0; margin-top: 3%; padding-bottom: 15px;"></div>';
     for (i=0; i<=numberOfProfiles; i++) {
         if (i%3===0) {
             $('.index-' + Math.floor(i/3)).wrapAll(span12);
