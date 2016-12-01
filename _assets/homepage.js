e12502ffc65c7321948eb22ad35c04c1436be69d
Lee <justinlee0022@gmail.com>
2016-12-01 08:58:15 -0800

    Removed unncessary javascript functions

--- _assets/homepage.js
+++ _assets/homepage.js
@@ -33,16 +33,6 @@ jQuery(document).ready(function ($) {
             }
         ]
     });
-   $( ".span4" ).each(function(index) {
-         $( this ).addClass("index-" + Math.floor(index/3));
-    });
-    var numberOfProfiles = $('.span4').length;
-    var span12 = '<div class="span12" style="margin-left: 0;"></div>';
-    for (i=0; i<=numberOfProfiles; i++) {
-        if (i%3===0) {
-            $('.index-' + Math.floor(i/3)).wrapAll(span12);
-        }
-    }
     $("img").attr("id", "featured");
     $('.events').slick({
         slide: 'li',
