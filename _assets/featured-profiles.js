40ccae6008bee5553a635f8406c48203bfc141fc
Victor Wu <victorwu-95@hotmail.com>
2017-01-11 09:07:14 -0800

    Remove unnecessary code

--- _assets/featured-profiles.js
+++ _assets/featured-profiles.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function ($) {
     var numofrows = document.getElementById("profiles").getElementsByClassName("span4");
     var profile_name;
     var profile_src;
-    var clone;
     var profile_pointer;
     $( ".span4" ).each(function(index) {
          $( this ).addClass("index-" + Math.floor(index/3));
@@ -18,7 +17,6 @@ jQuery(document).ready(function ($) {
         profile_pointer = document.getElementById(profile_name).getElementsByTagName("a");
         profile_src = $(profile_pointer[0]).attr("href");
         $("#" + profile_name + " > .profile-link").attr("href", profile_src);
-        console.log(profile_pointer);
     }
     $("img").attr("id", "featured");
 });
