48ad17a20ca34ea64f22412f741edf3cab8561c2
Victor Wu <victorwu-95@hotmail.com>
2017-01-11 10:31:39 -0800

    Add link to profile

--- _assets/homepage.js
+++ _assets/homepage.js
@@ -80,12 +80,26 @@ jQuery(document).ready(function ($) {
     var numOfNewsDisplay = 2;
     var i = 0;
     var news_src;
-    var test = document.getElementById("nonfeatured-news").getElementsByClassName("news");
+    var news_selector = document.getElementById("nonfeatured-news").getElementsByClassName("news");
     var news_name;
     for (i=0; i<numOfNewsDisplay; i++){
       news_name = "news_" + i.toString();
-      $(test[i]).attr("id", news_name);
+      $(news_selector[i]).attr("id", news_name);
       news_src = $("#nonfeatured-news > " + "#" + news_name + " > .not_used").text();
       $("#nonfeatured-news > " + "#" + news_name + " > a.post_link").attr("href", news_src);
+    }
+    
+    var index = 0;
+    var num_of_profiles = document.getElementById("profile_slider").getElementsByClassName("slick-list")[0].getElementsByClassName("slick-track")[0].children.length;
+    var profile_selector = document.getElementById("profile_slider").getElementsByClassName("slick-list")[0].getElementsByClassName("slick-track")[0].getElementsByClassName("slick-slide");
+    var profile_selector_src;
+    var profile_name;
+    var profile_src;
+    for (index=0; index<num_of_profiles; index++){
+        profile_name = "profile_" + index.toString();
+        $(profile_selector[index]).attr("id", profile_name);
+        profile_selector_src = document.getElementById(profile_name).getElementsByTagName("a")[0];
+        profile_src = $(profile_selector_src).attr("href");
+        $("#" + profile_name + " > .profile_link").attr("href", profile_src);
     }
 });
