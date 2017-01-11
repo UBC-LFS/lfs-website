1680253249dc62594ccaa835ab24af74148471ff
Justin Lee <justin.lee@ubc.ca>
2017-01-11 15:37:00 -0800

    ADDED: banner image and typed text for applied biology

--- _assets/applied-biology-program.js
+++ _assets/applied-biology-program.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function ($) {
     $(function () {
         $(".interests").typed({
-            strings: ["The interrelationships of plants, animals, soils and the atmosphere?", 
-                    "^750 Biodiversity?", 
-                    "^750 Sustainable food production systems?", 
-                    "^750 Protecting and improving the natural environment?",
-                    "^750 The health, protection and well-being of animals?", 
-                    "^750 Working to improve food production and solve food shortages?",
-                    "^750 Teaching others the intricacies and importance of the living world?",
-                    "^750 The role of agriculture in urban areas?",
+            strings: ["^750 the interrelationships of plants, animals, soils and the atmosphere?", 
+                    "^750 biodiversity?", 
+                    "^750 sustainable food production systems?", 
+                    "^750 protecting and improving the natural environment?",
+                    "^750 the health, protection and well-being of animals?", 
+                    "^750 working to improve food production and solve food shortages?",
+                    "^750 teaching others the intricacies and importance of the living world?",
+                    "^750 the role of agriculture in urban areas?",
                      ],
             typeSpeed: 20,
             backDelay: 1500,
