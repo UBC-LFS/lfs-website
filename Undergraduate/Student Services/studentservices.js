80edabd4c49f5710ea16a9e196b342d6a8020bed
Justin Lee <justin.lee@ubc.ca>
2017-01-12 15:37:34 -0800

    REMOVED: more css

--- Undergraduate/Student Services/studentservices.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-    $("div[role='main']").prepend($("#main-content"));
-    $("#main-content").prepend($("#content"));
-    $("#main-content").prepend($(".breadcrumb"));
-    $("#container").prepend($(".image-region"));
- 
-});
