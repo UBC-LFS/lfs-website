8ca0a47ff18d7be79b04617cf701ad418d09f1ba
Victor Wu <victorwu-95@hotmail.com>
2017-01-25 15:36:26 -0800

    ADDED: links to profile

--- Undergraduate/currentstudents.js
+++ Undergraduate/currentstudents.js
@@ -9,4 +9,15 @@ jQuery(document).ready(function ($) {
             $('.index-' + Math.floor(i/3)).wrapAll(span12);
         }
     }
+
+    var profile_count = $("#peers-profile > div.span12 > .featured-person").length;
+    var index;
+    var profile_src;
+    var profile_target;
+    for (index=0; index<profile_count; index++)
+    {
+        profile_target = document.getElementById("peers-profile").getElementsByClassName("featured-person")[index].getElementsByTagName("a");
+        profile_src = $(profile_target[0]).attr("href");
+        $(profile_target[1]).attr("href", profile_src);
+    }
 });
