864ed18c406cfc4709770dbd0f97bdc25206d386
Victor Wu <victorwu-95@hotmail.com>
2017-02-01 15:18:41 -0800

    REMOVED: comments and console.logs

--- Prospective/Undergraduate/prospective.js
+++ Prospective/Undergraduate/prospective.js
@@ -31,33 +31,19 @@ jQuery(document).ready(function ($) {
     count_down_detect();
     $(window).on('scroll',count_down_detect);
 
-    //var profile_count = $("#Faculty-0 .featured-person").length;
     var profile_count;
     var tab_count = $('#staff-profile > div.featured-group').length;
     var index;
     var indexj;
     var profile_src;
     var profile_target;
-    console.log("tab count:" + tab_count);
-    //profile_count = $('#staff-profile div.featured:nth-of-type(1) div.featured-person').length;
-    //console.log(profile_count);
     for (indexj=0;indexj<tab_count;indexj++)
     {
         profile_count = document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person').length;
-        console.log(profile_count);
-        //profile_count = $('#staff-profile .tab-content > div.tab-pane:nth-of-type(' + (indexj+1).toString() + ') div.featured-person').length;
-        //console.log(indexj);
-        // profile_count = $('#staff-profile > div.featured:nth-of-type(' + (indexj+1).toString() + ') .featured-person').length;
-        // console.log(profile_count);
         for (index=0; index<profile_count; index++)
         {
             profile_src = document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person')[index].getElementsByTagName('a')[0].href;
-            console.log(profile_src);
             document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person')[index].getElementsByTagName('a')[1].setAttribute('href',profile_src);
-            
-            // profile_src = $('#staff-profile > div.featured:nth-of-type(' + (indexj+1).toString() + ') .featured-person:nth-of-type(' + (index+1).toString() + ') > a:nth-of-type(1)').attr('href');
-            // $('#staff-profile > .featured:nth-of-type(' + (indexj+1).toString() + ') .featured-person:nth-of-type(' + (index+1).toString() + ') > a:nth-of-type(2)').attr('href', profile_src);
-            //console.log(profile_src);
         }
     }
 
