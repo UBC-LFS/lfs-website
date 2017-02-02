c78455aab6052a5578b5e67e7d305a6a54e3bd19
Victor Wu <victorwu-95@hotmail.com>
2017-02-02 11:26:59 -0800

    FIXED: Homepage news, call height function when reload

--- Home/home.js
+++ Home/home.js
@@ -128,50 +128,8 @@ jQuery(document).ready(function ($) {
         $('#nonfeatured-news').css('height',featured_news_height.toString() + 'px');
     }
 
-    function slick_profile_control_timeout()
-    {
-        setTimeout(slick_profile_control, 100);
-        // news_height_resize();
-    }
-    
-    function shiftLeft()
-    {
-        $('#profile_slider_prev .arrow-animation').stop(true,false);
-        $('#profile_slider_prev .arrow-animation').animate({
-            margin: '0px 0px 0px 0px'
-        }, 400);
-    }
-
-    function removeShiftLeft()
-    {
-        $('#profile_slider_prev .arrow-animation').stop(true,false);
-        $('#profile_slider_prev .arrow-animation').animate({margin: '0px 0px 0px 10px'}, 400);
-    }
-
-    shiftLeft();
-
-    slick_profile_control_timeout();
-    $(window).on('resize', slick_profile_control_timeout);
+    slick_profile_control();
+    news_height_resize();
+    $(window).on('resize', slick_profile_control);
     $(window).on('resize', news_height_resize);
-    $('#profile_slider_prev .arrow-animation p').on('mouseenter',shiftLeft);
-    $('#profile_slider_prev .arrow-animation p').on('mouseleave',removeShiftLeft);
-
-    // $('#profile_slider_prev > .arrow-animation > p').on({
-    //     mouseenter: function() {
-    //         $('#profile_slider_prev > .arrow-animation').animate({
-    //             margin: '0px 0px 0px 0px'
-    //         });
-    //     },
-    //     mouseleave: function() {
-    //         $('#profile_slider_prev > .arrow-animation').animate({
-    //             margin: '0px 0px 0px 10px'
-    //         });
-    //     }
-    // });
-    // function shiftLeft ()
-    // {
-    //     $('#profile_slider_prev > .arrow-animation').animate({
-    //         margin: '0px 0px 0px 0px';
-    //     });
-    // }
 });
