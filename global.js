1bf202d0957845b1526c7669f2aa67da71a47d13
Victor Wu <victorwu-95@hotmail.com>
2017-03-08 11:13:04 -0800

    ADDED: Global apply button

---
+++ global.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+    $("#ubc7-unit .container").prepend('<a class="apply-now-button" href="https://account.you.ubc.ca/ubc/apply/apply_now.ezc"><span>Apply to UBC</span> </a>');
+});
