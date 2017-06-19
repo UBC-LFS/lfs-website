8fb130146dd1f8e894b5148f4303476b5c390717
Justin Lee <justin.lee@ubc.ca>
2017-06-19 09:01:57 -0700

    Removed minifed news, not using news anymore

--- News/news_minified.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(e){var n,t,o=e("#non-featured-news-section > div.news").length;for(n=0;n<o;n++)t=e("#non-featured-news-section > div.news:nth-of-type("+(n+1).toString()+") > a:nth-of-type(1)").attr("href"),e("#non-featured-news-section > div.news:nth-of-type("+(n+1).toString()+") > a.non-featured-news").attr("href",t),console.log(t)});
