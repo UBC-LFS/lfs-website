61300d96879aafec143110e5275026b6a2e2310f
Justin Lee <justinlee0022@gmail.com>
2018-01-05 15:39:30 -0800

    Refactored colours, fixed featured profiles on prospective

--- Prospective/prospective.js
+++ Prospective/prospective.js
@@ -1,184 +1,32 @@
 /* global jQuery */
-jQuery(document).ready(function ($) {
-  $('.prospective-profile-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: false,
-    swipe: true,
-    arrows: true,
-    prevArrow: $('.slider-prev'),
-    nextArrow: $('.slider-next'),
-    responsive: [
-      {
-        breakpoint: 1024,
-        setting: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 769,
-        setting: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 481,
-        setting: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 1
-        }
-      }
-    ]
+document.addEventListener('DOMContentLoaded', function (event) {
+  var sources = document.getElementsByClassName('source-link')
+  var targets = document.getElementsByClassName('target-link')
+  Array.prototype.forEach.call(sources, function (source, i) {
+    var a = document.createElement('a')
+    a.href = source.children[0].href
+    a.appendChild(targets[i].children[0])
+    var wrapper = a.outerHTML
+    document.getElementsByClassName('target-link')[i].innerHTML = wrapper
   })
 
-  function setupSlickSlider () {
-    [].forEach.call($('.slider-wrap .slick-slide'), function (item, index, array) {
-      var slickSliderLink = $(item).children('a').eq(0).attr('href')
-      $(item).children('a').eq(1).attr('href', slickSliderLink)
-    })
-  }
-  setupSlickSlider()
-
   var animated = false
-  var topHeightToCounter = $('#ranking-world').offset().top
+  var topHeightToCounter = jQuery('#ranking-world').offset().top
 
   function countDownDetect () {
-    var yScrollPosition = window.pageYOffset + $(window).height() - $('#ranking-world').height() * 2 / 3
-    var yScrollPosition2 = window.pageYOffset - $('#ranking-world').height() * 2 / 5
+    var yScrollPosition = window.pageYOffset + jQuery(window).height() - jQuery('#ranking-world').height() * 2 / 3
+    var yScrollPosition2 = window.pageYOffset - jQuery('#ranking-world').height() * 2 / 5
     if (yScrollPosition > topHeightToCounter && yScrollPosition2 < topHeightToCounter && !animated) {
       var rankingWorld = new CountUp('ranking-world', 100, 25)
       var rankingCanada = new CountUp('ranking-canada', 100, 2, 0, 3)
-      $('#ranking-world').scroll(rankingWorld.start())
-      $('#ranking-canada').scroll(rankingCanada.start())
+      jQuery('#ranking-world').scroll(rankingWorld.start())
+      jQuery('#ranking-canada').scroll(rankingCanada.start())
       animated = true
     }
   }
   countDownDetect()
-  $(window).on('scroll', countDownDetect)
-
-  for (let indexj = 0; indexj < $('#staff-profile > div.featured-group').length; indexj++) {
-    var profileCount = document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person').length
-    for (let index = 0; index < profileCount; index++) {
-      var profileSource = document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person')[index].getElementsByTagName('a')[0].href
-      document.getElementById('staff-profile').getElementsByClassName('featured-group')[indexj].getElementsByClassName('featured-person')[index].getElementsByTagName('a')[1].setAttribute('href', profileSource)
-    }
-  }
-
-  // animate scroll to ids
-  $('#admissions-button').click(function () {
-    $('html, body').animate({
-      scrollTop: $('#admissions').offset().top
-    }, 1000)
-  })
-  $('#degree-button').click(function () {
-    $('html, body').animate({
-      scrollTop: $('#degree-at-a-glance').offset().top
-    }, 1000)
-  })
-  $('#outcomes-button').click(function () {
-    $('html, body').animate({
-      scrollTop: $('#outcomes').offset().top
-    }, 1000)
-  })
-
-  function alertAppend () {
-    var webAlertDisplayCheck = false    // checks if webpage has alerts
-    var pageAlertCount = 0
-
-    function pageAlertCounter () {
-      var pageAlertPCount = $('p.alert-text').length
-      var pageAlertText = $('p.alert-text')
-      var pageAlertTextCount = 0
-      var pageAlertNum = 0
-
-      for (let k = 0; k < pageAlertPCount; k++) {
-        pageAlertTextCount = $.trim(pageAlertText.eq(k).text()).length
-        if (pageAlertTextCount > 0) {
-          pageAlertNum++
-        }
-      }
-      return pageAlertNum
-    }
-    pageAlertCount = pageAlertCounter()
-
-    function pageAlertChecker () {
-      var j = 0  // counter used to count for the page-alert boxes
-
-      if (pageAlertCount > 0) {
-        webAlertDisplayCheck = true
-        // if there is more than 1 page-alert, clone the additional page alerts
-        for (let i = 1; i < pageAlertCount; i++) {
-          $('.website-alerts-container .page-alert').eq(0).clone().appendTo('.website-alerts-container')
-        }
-        // appends text to page-alert box
-        // this block of code prevents bugs that could occur if user deletes alert-1 and starts at alert-2
-        for (let k = 0; k < $('p.alert-text').length; k++) {
-          if ($.trim($('p.alert-text').eq(k).text()).length > 0) {
-            let alertText = $('p.alert-text').eq(k).text()
-            $('.website-alerts-container .page-alert .website-content-text').eq(j).text(alertText)
-            j++
-          }
-          if (j >= pageAlertCount) {
-            break
-          }
-        }
-      } else {
-        $('.website-alerts-container .page-alert').remove()
-      }
-      $('.website-alert-texts').remove()
-    }
-    pageAlertChecker()
-
-    function websiteAlertStyling () {
-      if ($('.website-alert').length > 0) {
-        $('.website-alerts-container').css('padding-bottom', '5px')
-      }
-      if (webAlertDisplayCheck === true) {
-        $('.website-alerts-container').css('display', 'block')
-      }
-    }
-    websiteAlertStyling()
-  }
-  alertAppend()
-
-  $('.close').click(function () {
-    $(this).parents('.website-alert').animate({height: '0px'}, function () {
-      this.remove()
-      if ($('.website-alert').length === 0) {
-        $('.website-alerts-container').animate({padding: '0px'}, function () {
-          $('.website-alerts-container').remove()
-        })
-      }
-    })
-  })
-
-  function alertPosition () {
-    var pageContentWidth = $('#main-content').width()
-    var websiteAlertContainerLeft = ($('#main-content').outerWidth(true) - $('#main-content').width()) / 2
-
-    $('.website-alerts-container').css('width', pageContentWidth + 'px')
-    $('.website-alerts-container').css('left', websiteAlertContainerLeft + 'px')
-  }
-  alertPosition()
-
-  function timeOutFncCall () {
-    alertPosition()
-  }
-
-  $(window).on('resize', function () {
-    setTimeout(timeOutFncCall, 100)
-  })
+  jQuery(window).on('scroll', countDownDetect)
 })
-
 /*
 
     countUp.js
