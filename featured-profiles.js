ba6b8e40051559fb4a6dea43a4a8f18fd0f89c36
Justin Lee <justinlee0022@gmail.com>
2018-01-09 14:59:26 -0800

    Updated featured profile page

--- featured-profiles.js
+++ featured-profiles.js
@@ -1,21 +1,11 @@
-/* global jQuery */
-jQuery(document).ready(function ($) {
-  var span12 = '<div class="span12 border-bottom" style="margin-left: 0; margin-top: 3%; padding-bottom: 15px;"></div>'
-  var numofrows = document.getElementById('profiles').getElementsByClassName('span4')
-
-  $('.span4').each(function (index) {
-    $(this).addClass('index-' + Math.floor(index / 3))
+document.addEventListener('DOMContentLoaded', function () {
+  var sources = document.getElementsByClassName('source-link')
+  var targets = document.getElementsByClassName('target-link')
+  Array.prototype.forEach.call(sources, function (source, i) {
+    var a = document.createElement('a')
+    a.href = source.children[0].href
+    a.appendChild(targets[i].children[0])
+    var wrapper = a.outerHTML
+    document.getElementsByClassName('target-link')[i].innerHTML = wrapper
   })
-
-  for (let i = 0; i < $('.span4').length; i++) {
-    if (i % 3 === 0) {
-      $('.index-' + Math.floor(i / 3)).wrapAll(span12)
-    }
-    var profileName = 'profile_' + i.toString()
-    $(numofrows[i]).attr('id', profileName)
-    var profilePointer = document.getElementById(profileName).getElementsByTagName('a')
-    var profileSrc = $(profilePointer[0]).attr('href')
-    $('#' + profileName + ' > .profile-link').attr('href', profileSrc)
-  }
-  $('img').attr('id', 'featured')
 })
