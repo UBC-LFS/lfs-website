1e2d350522b46418c3c4e0b4c370abb81fc1142a
Justin Lee <justinlee0022@gmail.com>
2018-01-10 14:23:21 -0800

    Changed out banner images

--- Undergraduate/Programs/applied-biology-program.js
+++ Undergraduate/Programs/applied-biology-program.js
@@ -1,18 +1,5 @@
 /* global jQuery */
 jQuery(document).ready(function ($) {
-  // $(function () {
-  //   $('.interests').typed({
-  //     strings: [ '^750 the health, protection and well-being of animals?',
-  //       '^750 biodiversity?',
-  //       '^750 sustainable food production systems?'
-  //     ],
-  //     typeSpeed: 20,
-  //     backDelay: 1500,
-  //     backSpeed: -20,
-  //     loop: false
-  //   })
-  // })
-
   var sources = document.getElementsByClassName('source-link')
   var targets = document.getElementsByClassName('target-link')
   Array.prototype.forEach.call(sources, function (source, i) {
