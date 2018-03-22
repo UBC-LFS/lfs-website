a0965b84bfbc38d1cb365f2211fe0e7dd4087d2d
Justin Lee <justinlee0022@gmail.com>
2018-03-22 08:55:33 -0700

    Added featured profiles to prospective

--- prospective/prospective.js
+++ prospective/prospective.js
@@ -1,14 +1,15 @@
 /* global jQuery */
 document.addEventListener('DOMContentLoaded', function (event) {
-  // var sources = document.getElementsByClassName('source-link')
-  // var targets = document.getElementsByClassName('target-link')
-  // Array.prototype.forEach.call(sources, function (source, i) {
-  //   var a = document.createElement('a')
-  //   a.href = source.children[0].href
-  //   a.appendChild(targets[i].children[0])
-  //   var wrapper = a.outerHTML
-  //   document.getElementsByClassName('target-link')[i].innerHTML = wrapper
-  // })
+  var sources = document.getElementsByClassName('source-link')
+  var targets = document.getElementsByClassName('target-link')
+  Array.from(sources).forEach(function (source, i) {
+    var a = document.createElement('a')
+    a.href = source.children[0].href
+    a.appendChild(targets[i].children[1])
+    console.log(a)
+    var wrapper = a.outerHTML
+    document.getElementsByClassName('target-link')[i].innerHTML = wrapper
+  })
 
   var animated = false
   var topHeightToCounter = jQuery('#ranking-world').offset().top
