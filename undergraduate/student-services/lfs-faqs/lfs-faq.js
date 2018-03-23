9d8b3670eacf319e8ef5d9b29e318dc36bd8b660
Justin Lee <justinlee0022@gmail.com>
2018-03-23 09:37:39 -0700

    Built accordions for lfs faq page

---
+++ undergraduate/student-services/lfs-faqs/lfs-faq.js
@@ -0,0 +1,14 @@
+/* global jQuery */
+jQuery(document).ready(function ($) {
+  var arrow = '<i class="icon-chevron-down"></i>'
+  $('.accordion-toggle').append(arrow)
+
+  // var accordionHeadings = Array.from(document.getElementsByClassName('accordion-heading'))
+  // var chevronDown = Array.from(document.getElementsByClassName('icon-chevron-down'))
+  // accordionHeadings.forEach((ah, i) => 
+  //   ah.addEventListener('click', () => {
+  //     console.log(chevronDown)
+  //     chevronDown[i].className = 'icon-chevron-up'
+  //   })
+  // )
+})
