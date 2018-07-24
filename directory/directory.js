93ae4629a4f6c6d489b1dc033ac86b1ca9513722
Justin Lee <justinlee0022@gmail.com>
2018-07-24 15:10:32 -0700

    Modified code to work with old IE

--- directory/directory.js
+++ directory/directory.js
@@ -1,49 +1,43 @@
+'use strict';
+
 /* global XMLHttpRequest, jQuery */
 
 jQuery(document).ready(function ($) {
-  const generateTable = (staffList) => {
-    const cleanStaffList = staffList.map(staff => ({
-      firstName: staff.givenName,
-      lastName: staff.sn,
-      phone: Array.isArray(staff.telephoneNumber) ? staff.telephoneNumber[0] : staff.telephoneNumber,
-      email: staff.mail,
-      office: Array.isArray(staff.l) ? staff.l[0] : staff.l,
-      title: staff.title
-    }))
-    const dataForTable = cleanStaffList.map(staff => {
-      const email = staff => `<a href='mailto:${staff.email}'>${staff.email}</a>`
-      return [
-        staff.lastName,
-        staff.firstName,
-        staff.title,
-        email(staff),
-        staff.office,
-        staff.phone
-      ]
-    })
+  var generateTable = function generateTable(staffList) {
+    var cleanStaffList = staffList.map(function (staff) {
+      return {
+        firstName: staff.givenName,
+        lastName: staff.sn,
+        phone: Array.isArray(staff.telephoneNumber) ? staff.telephoneNumber.join('<br>') : staff.telephoneNumber,
+        email: Array.isArray(staff.mail) ? staff.mail : [staff.mail],
+        office: Array.isArray(staff.l) ? staff.l.join('<br>') : staff.l,
+        title: staff.title
+      };
+    });
+    var dataForTable = cleanStaffList.map(function (staff) {
+      var email = function email(staff) {
+        return staff.email.map(function (email) {
+          return '<a href=\'mailto:' + email + '\'>' + email + '</a><br>';
+        });
+      };
+      return [staff.lastName, staff.firstName, staff.title, email(staff), staff.office, staff.phone];
+    });
     $('#staffList').DataTable({
       data: dataForTable,
-      columns: [
-        { title: 'Last Name', 'defaultContent': '' },
-        { title: 'First Name', 'defaultContent': '' },
-        { title: 'Title', 'defaultContent': '' },
-        { title: 'Email', 'defaultContent': '' },
-        { title: 'Office', 'defaultContent': '' },
-        { title: 'Phone', 'defaultContent': '' }
-      ],
+      columns: [{ title: 'Last Name', 'defaultContent': '' }, { title: 'First Name', 'defaultContent': '' }, { title: 'Title', 'defaultContent': '' }, { title: 'Email', 'defaultContent': '' }, { title: 'Office', 'defaultContent': '' }, { title: 'Phone', 'defaultContent': '' }],
       'pageLength': 100
-    })
-  }
-  const xhr = new XMLHttpRequest()
+    });
+  };
+  var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      const staffList = JSON.parse(xhr.responseText)
-      console.log(staffList)
-      generateTable(staffList)
+      var staffList = JSON.parse(xhr.responseText);
+      console.log(staffList);
+      generateTable(staffList);
     }
-  }
+  };
 
-  xhr.open('GET', 'http://prod-lc01-pub.landfood.ubc.ca/lfsdir', true)
-  xhr.send()
-})
+  xhr.open('GET', 'http://prod-lc01-pub.landfood.ubc.ca/lfsdir', true);
+  xhr.send();
+});
